260536aeb9f03f21f45eeca3800b2d66a0bb4902
keshav <keshavchauhan1112@gail.com>
2025-06-20 10:36:56 +0000

    [automatic commit] 2025-06-20 10:36:56 UTC 129.127.202.33

--- part2/middleware/auth.js
+++ part2/middleware/auth.js
@@ -8,7 +8,7 @@ function ensureLoggedIn(req,res,next){
 function ensureRole(role){
     return function(req,res,next){
         if(!req.session.user || req.session.user.role !== role){
-            
+            return res.status(403).send('Forbidden')
         }
     }
 }
