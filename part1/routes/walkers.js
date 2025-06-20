d28104460fe318aca0b961d09ec458121c591d45
keshav <keshavchauhan1112@gail.com>
2025-06-20 04:30:22 +0000

    fghjk

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -7,3 +7,14 @@ module.exports = (db) => {
 
  router.get('/summary', async (req, res) => {
    try {
+    const [rows] = await db.query(`
+       SELECT
+         u.username AS walker_username,
+         COUNT(CASE WHEN wr.status = 'completed' THEN 1 END) AS completed_walks,
+         ROUND(AVG(r.rating), 2) AS average_rating
+       FROM Users u
+       LEFT JOIN WalkRatings r ON u.user_id = r.walker_id
+       LEFT JOIN WalkRequests wr ON r.request_id = wr.request_id
+       WHERE u.role = 'walker'
+       GROUP BY u.user_id
+     `);
