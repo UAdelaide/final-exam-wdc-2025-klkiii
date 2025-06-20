ba597362de0078f034cad41368d5fad8651c350b
keshav <keshavchauhan1112@gail.com>
2025-06-20 12:02:48 +0000

    [automatic commit] 2025-06-20 12:02:48 UTC 129.127.202.33

--- part2/routes/api.js
+++ part2/routes/api.js
@@ -2,4 +2,6 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db.js');
 
-router.get('/dogs',async)
+router.get('/dogs',async (req,res)=>{
+    
+})
