a615375ed05229b9438d108999db49b7f8b02418
keshav <keshavchauhan1112@gail.com>
2025-06-20 07:02:06 +0000

    [automatic commit] 2025-06-20 07:02:06 UTC 129.127.202.33

--- part1/public/javascripts/script.js
+++ part1/public/javascripts/script.js
@@ -1,58 +1,65 @@
-const { createApp, onMounted, ref} = Vue;
+const { createApp } = Vue;
 
 createApp({
-    data(){
-        return {
-        dogImage:'',
-        showBookingStep2: false,
-        cursor:{x:0,y:0},
-        buttons:[]
+  data() {
+    return {
+      dogImage: '',
+      showBookingStep2: false,
+      cursor: { x: 0, y: 0 },
+      buttons: []
     };
-},
+  },
 
-methods:{
-    fetchDog(){
-        fetch("https://dog.ceo/api/breeds/image/random")
-        .then(res=> res.json())
-        .then(data=>{
-            this.dogImage = data.message;
+  methods: {
+    fetchDog() {
+      fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(data => {
+          this.dogImage = data.message;
         });
     },
-    multiStepRefresh(){
-        if (confirm("Are you sure you want to relod the dog image?")){
-            this.fetchDog();
-        }
-    },
-    startBooking(){
-        this.showBookingStep2 = true;
-    },
-    confirmBooking(){
-        alert("Booking confirmed!")
-    },
-    moveButtonIfClose(btn){
-        const rect = btn.getBoundingClientRect();
-        const distX = this.cursor.x - (rect.left+rect.width / 2);
-        const distY = this.cursor.y - (rect.top +rect.height / 2);
-        const distance = Math.sqrt(distX**2 + distY ** 2);
 
-        if (distance < 100){
-            const offsetX = (Math.random()-0.5) * 300;
-            const offsetY = (Math.random() - 0.5) *300;
-            btn.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
-        }
+    multiStepRefresh() {
+      if (confirm("Are you sure you want to reload the dog image?")) {
+        this.fetchDog();
+      }
+    },
+
+    startBooking() {
+      this.showBookingStep2 = true;
+    },
+
+    confirmBooking() {
+      alert("Booking confirmed!");
+    },
+
+    moveButtonIfClose(btn) {
+      const rect = btn.getBoundingClientRect();
+      const distX = this.cursor.x - (rect.left + rect.width / 2);
+      const distY = this.cursor.y - (rect.top + rect.height / 2);
+      const distance = Math.sqrt(distX ** 2 + distY ** 2);
+
+      if (distance < 100) {
+        const offsetX = (Math.random() - 0.5) * 300;
+        const offsetY = (Math.random() - 0.5) * 300;
+        btn.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+      }
+    },
+
+    trackMouse(e) {
+      this.cursor.x = e.clientX;
+      this.cursor.y = e.clientY;
+      this.buttons.forEach(btn => this.moveButtonIfClose(btn));
     }
-},
-tarckMouse(e){
-    this.cursor.x = e.clientX;
-    this.cursor.y = e.clientY;
-    this.buttons.forEach(this.moveButtonIfClose);
-},
-mounted(){
+  },
+
+  mounted() {
     this.fetchDog();
-    this.button = Array.from(document.querySelectorAll('.runaway'));
+    this.buttons = Array.from(document.querySelectorAll('.runaway'));
     window.addEventListener("mousemove", this.trackMouse);
-},
-beforeUnmount(){
-    window.removeEventlistner("mousemove",this.trackMouse);
-}
+  },
+
+  beforeUnmount() {
+    window.removeEventListener("mousemove", this.trackMouse);
+  }
 }).mount('#app');
