2d432b554db1fc38a70ddfdb92051f4d8031a8d9
keshav <keshavchauhan1112@gail.com>
2025-06-20 04:25:18 +0000

    [automatic commit] 2025-06-20 04:25:18 UTC 129.127.202.33

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -3,3 +3,9 @@ const express = require('express');
 
 module.exports = (db) => {
  const router = express.Router();
+
+
+
+ router.get('/', async (req, res) => {
+   try {
+     const [rows] = await db.query(`
