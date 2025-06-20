17c52f2161252e218a28cbcebdb6e609f8ff5df9
keshav <keshavchauhan1112@gail.com>
2025-06-20 13:28:58 +0000

    [automatic commit] 2025-06-20 13:28:58 UTC 129.127.202.33

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -39,23 +39,44 @@ router.get('/me', (req, res) => {
 //didnt use this pls reffer to login.js file in routes folder
 // i realised later that it exists
 // POST login (dummy version)
-router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
+router.post('/login',async(req,res)=>{
+    const { username , password } = req.body;
 
-  try {
-    const [rows] = await db.query(`
-      SELECT user_id, username, role FROM Users
-      WHERE email = ? AND password_hash = ?
-    `, [email, password]);
+    try{
+        //lookup user
+        const [users] = await db.query(
+            'SELECT * FROM Users Where username = ? AND password_hash = ?',
+            [username, password]
+        );
+        if (users.length === 0){
+            return res.status(401).send('Invalid credentials');
+        }
 
-    if (rows.length === 0) {
-      return res.status(401).json({ error: 'Invalid credentials' });
+        //save session
+        req.session.user = {
+            id: users[0].user_id,
+            username: users[0].username,
+            role: users[0].role
+        };
+
+        // console.log(users[0].role);
+
+        //redirect based on role
+        if (users[0].role === 'owner'){
+            return res.redirect('/owner-dashboard');
+        }
+        if (users[0].role === 'walker'){
+            return res.redirect('/walker-dashboard');
+        }
+        return res.status(403).send('Unknown role');
+
+
+
+    } catch(err){
+        console.error(err);
+        return res.status(500).send('Internal server error');
     }
 
-    res.json({ message: 'Login successful', user: rows[0] });
-  } catch (error) {
-    res.status(500).json({ error: 'Login failed' });
-  }
 });
 
 module.exports = router;
