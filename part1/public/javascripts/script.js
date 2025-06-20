94a92f30947cf53afe6652deb245d54227523e36
keshav <keshavchauhan1112@gail.com>
2025-06-20 07:11:00 +0000

    [automatic commit] 2025-06-20 07:11:00 UTC 129.127.202.33

--- part1/public/javascripts/script.js
+++ part1/public/javascripts/script.js
@@ -1,71 +1,58 @@
-const { createApp } = Vue;
+const { createApp, onMounted, ref} = Vue;
 
 createApp({
-  data() {
-    return {
-      dogImage: '',
-      showBookingStep2: false,
-      cursor: { x: 0, y: 0 },
-      buttons: []
+    data(){
+        return {
+        dogImage:'',
+        showBookingStep2: false,
+        cursor:{x:0,y:0},
+        buttons:[]
     };
-  },
+},
 
-  methods: {
-    fetchDog() {
-      fetch("https://dog.ceo/api/breeds/image/random")
-        .then(res => res.json())
-        .then(data => {
-          this.dogImage = data.message;
+methods:{
+    fetchDog(){
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res=> res.json())
+        .then(data=>{
+            this.dogImage = data.message;
         });
     },
-
-    multiStepRefresh() {
-      if (confirm("Are you sure you want to reload the dog image?")) {
-        this.fetchDog();
-      }
+    multiStepRefresh(){
+        if (confirm("Are you sure you want to relod the dog image?")){
+            this.fetchDog();
+        }
     },
-
-    startBooking() {
-      this.showBookingStep2 = true;
+    startBooking(){
+        this.showBookingStep2 = true;
     },
-
-    confirmBooking() {
-      alert("Booking confirmed!");
+    confirmBooking(){
+        alert("Booking confirmed!")
     },
+    moveButtonIfClose(btn){
+        const rect = btn.getBoundingClientRect();
+        const distX = this.cursor.x - (rect.left+rect.width / 2);
+        const distY = this.cursor.y - (rect.top +rect.height / 2);
+        const distance = Math.sqrt(distX**2 + distY ** 2);
 
-    moveButtonIfClose(btn) {
-      const rect = btn.getBoundingClientRect();
-      const distX = this.cursor.x - (rect.left + rect.width / 2);
-      const distY = this.cursor.y - (rect.top + rect.height / 2);
-      const distance = Math.sqrt(distX ** 2 + distY ** 2);
-
-      if (distance < 100) {
-        const offsetX = (Math.random() - 0.5) * 300;
-        const offsetY = (Math.random() - 0.5) * 300;
-        btn.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
-        btn.style.transform = `translate(${offsetX}px, ${offsetY}px) rotate(${Math.random() * 360}deg)`;
-     }
-    //  if (Math.random() < 0.1) {
-    //     btn.style.position = "absolute";
-    //     btn.style.left = `${Math.random() * window.innerWidth}px`;
-    //     btn.style.top = `${Math.random() * window.innerHeight}px`;
-    //     }
-    },
-
-    trackMouse(e) {
-      this.cursor.x = e.clientX;
-      this.cursor.y = e.clientY;
-      this.buttons.forEach(btn => this.moveButtonIfClose(btn));
+        if (distance < 100){
+            const offsetX = (Math.random()-0.5) * 300;
+            const offsetY = (Math.random() - 0.5) *300;
+            btn.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+        }
     }
-  },
-
-  mounted() {
+},
+tarckMouse(e){
+    this.cursor.x = e.clientX;
+    this.cursor.y = e.clientY;
+    this.buttons.forEach(this.moveButtonIfClose);
+},
+mounted(){
     this.fetchDog();
-    this.buttons = Array.from(document.querySelectorAll('.runaway'));
+    this.button = Array.from(document.querySelectorAll('.runaway'));
     window.addEventListener("mousemove", this.trackMouse);
-  },
-
-  beforeUnmount() {
-    window.removeEventListener("mousemove", this.trackMouse);
-  }
+},
+beforeUnmount(){
+    window.removeEventlistner("mousemove",this.trackMouse);
+}
 }).mount('#app');
