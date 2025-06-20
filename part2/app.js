03c1602d896b7b4ed3137e510cfc63f8823d82e0
keshav <keshavchauhan1112@gail.com>
2025-06-20 13:33:24 +0000

    [automatic commit] 2025-06-20 13:33:24 UTC 129.127.202.33

--- part2/app.js
+++ part2/app.js
@@ -31,7 +31,7 @@ const dogRoute = require('./routes/dogs');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
-app.use('/',loginRoutes);
+// app.use('/',loginRoutes);
 app.use('/',logoutRoute);
 app.use('/',dogRoute);
 app.use('/api/dogs', require('./routes/dogs'));
