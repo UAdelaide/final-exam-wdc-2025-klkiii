d0b145636fa2ba27a08342ff811f8a8eebc62984
keshav <keshavchauhan1112@gail.com>
2025-06-20 09:52:03 +0000

    [automatic commit] 2025-06-20 09:52:03 UTC 129.127.202.33

--- part2/routes/login.js
+++ part2/routes/login.js
@@ -26,7 +26,7 @@ router.post('/login',async(req,res)=>{
         if (user.role === 'owner'){
             return res.redirect('/owner-dashboard.html');
         }
-        elseif (user[0].role === 'walker'){
+        else if (user[0].role === 'walker'){
             return res.redirect('/walker-dashboard.html');
         }
         else{
