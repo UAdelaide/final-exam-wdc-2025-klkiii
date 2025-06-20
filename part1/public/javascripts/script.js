92041996132d39bd4f0d5f0189d96a2c44094ab2
keshav <keshavchauhan1112@gail.com>
2025-06-20 06:24:15 +0000

    [automatic commit] 2025-06-20 06:24:15 UTC ;; communications error to 208.67.222.222#53: timed out 129.127.202.33

--- part1/public/javascripts/script.js
+++ part1/public/javascripts/script.js
@@ -13,8 +13,8 @@ methods:{
         fetch("https://dog.ceo/api/breeds/image/random")
         .then(res=> res.json())
         .then(data=>{
-            this.dogImage
-        })
+            this.dogImage = data.message;
+        });
     }
 }
 })
