7247c5a2189ee3054b1c4b2767ef1dad2a622d2a
keshav <keshavchauhan1112@gail.com>
2025-06-20 13:33:45 +0000

    [automatic commit] 2025-06-20 13:33:45 UTC 129.127.202.33

--- part2/routes/login.js
+++ part2/routes/login.js
@@ -1,45 +1,45 @@
-const express = require('express');
-const router = express.Router();
-const db = require('../models/db.js');
+// const express = require('express');
+// const router = express.Router();
+// const db = require('../models/db.js');
 
-router.post('/login',async(req,res)=>{
-    const { username , password } = req.body;
+// router.post('/login',async(req,res)=>{
+//     const { username , password } = req.body;
 
-    try{
-        //lookup user
-        const [users] = await db.query(
-            'SELECT * FROM Users Where username = ? AND password_hash = ?',
-            [username, password]
-        );
-        if (users.length === 0){
-            return res.status(401).send('Invalid credentials');
-        }
+//     try{
+//         //lookup user
+//         const [users] = await db.query(
+//             'SELECT * FROM Users Where username = ? AND password_hash = ?',
+//             [username, password]
+//         );
+//         if (users.length === 0){
+//             return res.status(401).send('Invalid credentials');
+//         }
 
-        //save session
-        req.session.user = {
-            id: users[0].user_id,
-            username: users[0].username,
-            role: users[0].role
-        };
+//         //save session
+//         req.session.user = {
+//             id: users[0].user_id,
+//             username: users[0].username,
+//             role: users[0].role
+//         };
 
-        // console.log(users[0].role);
+//         // console.log(users[0].role);
 
-        //redirect based on role
-        if (users[0].role === 'owner'){
-            return res.redirect('/owner-dashboard');
-        }
-        if (users[0].role === 'walker'){
-            return res.redirect('/walker-dashboard');
-        }
-        return res.status(403).send('Unknown role');
+//         //redirect based on role
+//         if (users[0].role === 'owner'){
+//             return res.redirect('/owner-dashboard');
+//         }
+//         if (users[0].role === 'walker'){
+//             return res.redirect('/walker-dashboard');
+//         }
+//         return res.status(403).send('Unknown role');
 
 
 
-    } catch(err){
-        console.error(err);
-        return res.status(500).send('Internal server error');
-    }
+//     } catch(err){
+//         console.error(err);
+//         return res.status(500).send('Internal server error');
+//     }
 
-});
+// });
 
-module.exports = router;
+// module.exports = router;
