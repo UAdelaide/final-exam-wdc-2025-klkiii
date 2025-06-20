4b77ee8bc199c8cf4799dc0b379ce87d4a494336
keshav <keshavchauhan1112@gail.com>
2025-06-20 09:19:05 +0000

    [automatic commit] 2025-06-20 09:19:05 UTC 129.127.202.33

--- part2/app.js
+++ part2/app.js
@@ -7,6 +7,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(session())
 
 //session
 app.use(express.urlencoded({extended:true}));
