0606b29b718890ca18f0a0028c91d8f90bef2d88
keshav <keshavchauhan1112@gail.com>
2025-06-20 04:32:56 +0000

    fghjk

--- part1/routes/walkrequests.js
+++ part1/routes/walkrequests.js
@@ -4,4 +4,10 @@ router.get('/open', async (req, res) => {
    try {
      const [rows] = await db.query(`
        SELECT wr.requested_time, wr.location, d.name AS dog_name, u.username AS owner_username
+       FROM WalkRequests wr
+       JOIN Dogs d ON wr.dog_id = d.dog_id
+       JOIN Users u ON d.owner_id = u.user_id
+       WHERE wr.status = 'open'
+     `);
+
 
